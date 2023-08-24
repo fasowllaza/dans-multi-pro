d7e74968242b1392b7bd73a6b8d0113a38181715
Fakhreza Dwiputra Suparyadi <52316053+fasowllaza@users.noreply.github.com>
2023-08-24 18:37:49 +0700

    [Setup] Setup and preparing database

---
+++ controllers/user.js
@@ -0,0 +1,66 @@
+const {User} = require("../models")
+const {decode} = require("../helpers/bcrypt")
+const {sign} = require("../helpers/jwt")
+
+class Controller{
+    static registerUser(req, res, next) {
+        let userData = {
+            username:req.body.username,
+            password:req.body.password
+        }
+        User
+        .create(userData)
+        .then((data)=>{
+            res.status(201).json({
+                name:data.name,
+                msg:"Account Created"
+            })
+        })
+        .catch((err)=>{
+            if(err.name==="ServerError"){
+                next({name: "ServerError", message:err.message})
+            }
+            else{
+                next({name: "SequelizeValidationError", err})
+            }
+        })
+    }
+    static loginUser(req, res, next){
+        let userData = {
+            username:req.body.username,
+            password:req.body.password,
+        }
+        User
+        .findOne({
+            where:{
+                username: userData.username
+            }
+        })
+        .then((data)=>{
+            if(data){
+                if(decode(userData.password,data.password)){
+                    let payload = {
+                        name:data.name,
+                        email:data.email,
+                        role:data.role,
+                        id:data.id,
+                        verified:data.verified
+                    }
+                    const access_token = sign(payload)
+                    res.status(200).json({access_token})
+                }
+                else{
+                    next({name:"BadRequest", message:"Invalid username/password"})
+                }
+            }
+            else{
+                next({name:"BadRequest", message:"Invalid username/password"})
+            }
+        })
+        .catch((err)=>{
+            next({name: "ServerError", message:err.message})
+        })
+    }
+}
+
+module.exports = Controller
